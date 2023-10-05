d011b53ce661c044a8a969eaa85d61372735093f
Sean McGettrick <smcgettrick@gmail.com>
2023-10-05 11:19:22 -0400

    Some additional comments

--- tests/SdmCo.Reddit.Monitor.UnitTests/Mocks/MockHttpMessageHandler.cs
+++ tests/SdmCo.Reddit.Monitor.UnitTests/Mocks/MockHttpMessageHandler.cs
@@ -2,11 +2,14 @@
 
 public class MockHttpMessageHandler : HttpMessageHandler
 {
+    // Function to represent code to generate a custom HttpResponseMessage
     private readonly Func<HttpRequestMessage, CancellationToken, HttpResponseMessage> _responseGenerator;
 
+    // Create a function to generate an HttpResonseMessage based on the HttpRequestMessage passed in
     public MockHttpMessageHandler(Func<HttpRequestMessage, CancellationToken, HttpResponseMessage> responseGenerator) =>
         _responseGenerator = responseGenerator ?? throw new ArgumentNullException(nameof(responseGenerator));
 
+    // Overrides HttpClient SendAsync to return a pre-defined HttpResponseMessage
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
         CancellationToken cancellationToken) => Task.FromResult(_responseGenerator(request, cancellationToken));
 }
